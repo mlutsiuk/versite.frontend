87e2c255c9def51dec173cbe9970532a9756f208
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-10-28 05:59:50 +0300

    feat: Add index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-red">
-    test bg-red
+  <div class="min-h-[300vh] container mx-auto">
+    <h1 class="text-2xl font-bold text-indigo-500">Hello World</h1>
   </div>
 </template>
 
