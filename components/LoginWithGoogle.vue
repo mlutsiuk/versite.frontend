ecd98e437a842dc2de67a9b760b508e581a15836
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-11-01 04:09:20 +0200

    feat: Add Google login button

---
+++ components/LoginWithGoogle.vue
@@ -0,0 +1,70 @@
+<template>
+  <button @click="login" class="flex items-center justify-center w-full px-4 h-[60px] font-normal rounded-md bg-gray-50">
+    <span class="flex items-center text-gray-600">
+      <span class="font-bold text-2xl">G</span>
+
+      <span>&nbsp;Google</span>
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '~~/store/auth';
+import Cookies from "js-cookie";
+
+const authStore = useAuthStore();
+const config = useRuntimeConfig();
+const GOOGLE_CALLBACK_MESSAGE_EVENT = 'versite@auth-google-callback';
+
+async function login() {
+  const { data, error } = await useFetch<{url: string}>('http://localhost:8080/v1/auth/login/google/redirect');
+  openWindow(data.value.url, 'Google Login');
+}
+
+function openWindow(url, title) {
+    let options = {
+      url,
+      title,
+      width: window.screen.width / 2,
+      height: window.screen.height / 2,
+      left: window.screen.width / 4,
+      top: window.screen.height / 8
+    };
+    const optionsStr = Object.keys(options).reduce((acc, key) => {
+      return `${acc}${key}=${options[key]},`;
+    }, '');
+    optionsStr.slice(0, -1);    // Removing last comma
+    let loginWindow = window.open(
+      url,
+      title,
+      optionsStr
+    );
+
+    if(window.focus) {
+      loginWindow.focus();
+    }
+}
+
+function onMessage(e: MessageEvent) {
+  if(e.origin !== config.public.apiBase) { return; }
+  if(e.data.source !== GOOGLE_CALLBACK_MESSAGE_EVENT) { return; }
+
+  authStore.saveToken(e.data.payload.access_token);
+  authStore.fetchUser();
+
+  const intendedUrl = Cookies.get('intended_url');
+  if(intendedUrl) {
+    Cookies.remove('intended_url');
+    navigateTo(intendedUrl);
+  } else {
+    navigateTo('/');
+  }
+}
+
+onMounted(() =>{
+  window.addEventListener('message', onMessage, false);
+});
+onBeforeUnmount(() => {
+  window.removeEventListener('message', onMessage);
+});
+</script>
