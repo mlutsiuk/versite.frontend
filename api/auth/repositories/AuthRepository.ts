dab4a3f007b28361b1148703d1a8b4c48c783777
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-11-04 06:30:20 +0200

    refactor: AuthRepository format

--- api/auth/repositories/AuthRepository.ts
+++ api/auth/repositories/AuthRepository.ts
@@ -1,22 +1,16 @@
 import { AccessTokenResponse, GetGoogleLoginUrlResponse, PasswordLoginRequest, ResponseSingleResource } from "~/api";
 import { UserModel } from "~/api";
 
-async function getAuthenticatedUser() {
-  return await useHttpGet<ResponseSingleResource<UserModel>>('v1/auth/profile');
-}
-
-async function getGoogleLoginUrl() {
-  return await useHttpGet<GetGoogleLoginUrlResponse>('v1/auth/login/google/redirect');
-}
-
-async function passwordLogin(body: PasswordLoginRequest) {
-  return await useHttpPost<AccessTokenResponse>('v1/auth/login/password', {
-    body
-  });
-}
-
 export const authRepository = {
-  getAuthenticatedUser,
-  getGoogleLoginUrl,
-  passwordLogin
+  async getAuthenticatedUser() {
+    return await useHttpGet<ResponseSingleResource<UserModel>>('v1/auth/profile');
+  },
+  async getGoogleLoginUrl() {
+    return await useHttpGet<GetGoogleLoginUrlResponse>('v1/auth/login/google/redirect');
+  },
+  async passwordLogin(body: PasswordLoginRequest) {
+    return await useHttpPost<AccessTokenResponse>('v1/auth/login/password', {
+      body
+    });
+  }
 }
