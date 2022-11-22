30313c86234c33e973ef2ac93a361f9d33ab5e74
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-11-22 04:42:11 +0200

    feat: Add Icon component

---
+++ components/Markdown/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <Icon
+    :icon="props.icon"
+    :inline="props.inline"
+    :width="props.width"
+  />
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue/dist/offline';
+import type { IconifyIcon } from '@iconify/types';
+
+const props = withDefaults(defineProps<{
+  icon: IconifyIcon,
+  width?: number,
+  inline?: boolean
+}>(), {
+  width: 24,
+  inline: false
+});
+</script>
