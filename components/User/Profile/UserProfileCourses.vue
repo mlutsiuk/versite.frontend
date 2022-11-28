8ea758745118118da233eaed1f8c3d83a601f9c8
Anna Romaniuk <80681158+AnechkaR@users.noreply.github.com>
2022-11-28 06:32:18 +0200

    Add courses index

--- components/User/Profile/UserProfileCourses.vue
+++ components/User/Profile/UserProfileCourses.vue
@@ -5,18 +5,25 @@
     </div>
 
     <div class="grid grid-cols-2 gap-4 mt-4">
-      <div
+      <NuxtLink
         v-for="id in 4"
-        class="flex flex pr-2 border rounded"
+        v-slot="{ navigate }"
+        to="/courses/advanced-uiux-design-in-figma"
+        custom
       >
-        <div class="bg-violet-100 h-20 w-16"></div>
+        <div
+          @click="navigate"
+          class="flex flex pr-2 border rounded select-none cursor-pointer"
+        >
+          <div class="bg-violet-100 h-20 w-16"></div>
 
-        <div class="ml-2">
-          <div class="font-semibold">Advanced UI/UX Design in Figma</div>
-          <div class="text-gray-500">Learn new powerful techniques to create amazing user experience</div>
+          <div class="ml-2">
+            <div class="font-semibold">Advanced UI/UX Design in Figma</div>
+            <div class="text-gray-500">Learn new powerful techniques to create amazing user experience</div>
+          </div>
+
         </div>
-
-      </div>
+      </NuxtLink>
     </div>
   </div>
 </template>
---
+++ pages/courses/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <UserProfileCourses/>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
