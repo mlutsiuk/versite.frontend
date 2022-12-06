edcbf22c341ef6b7ec68813e860ca6e79dd04015
Anna Romaniuk <80681158+AnechkaR@users.noreply.github.com>
2022-12-06 16:13:31 +0200

    Add course rating page

---
+++ pages/courses/[slug]/rating.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-row h-full space-x-5">
+    <div class="basis-2/12 h-full bg-violet-100">
+      <CourseSidebar :course-slug="$route.params.slug"/>
+    </div>
+
+    <div class="basis-10/12">
+
+    </div>
+  </div>
+</template>
