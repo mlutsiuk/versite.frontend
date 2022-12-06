5d2506ee5f247525d05a0e5cf8d4651447a2ec4f
Anna Romaniuk <80681158+AnechkaR@users.noreply.github.com>
2022-12-06 16:12:28 +0200

    Add course landing stats

--- pages/courses/[slug]/index.vue
+++ pages/courses/[slug]/index.vue
@@ -1,23 +1,44 @@
 <template>
-  <div class="flex flex-row space-x-5">
-    <div class="basis-5/12 h-screen bg-violet-100">
+  <div class="flex space-x-5">
+    <div class="sticky top-0 w-5/12 h-screen bg-violet-100">
       <div class="text-2xl font-semibold select-none cursor-pointer mt-4 ml-16">
         Versite
       </div>
     </div>
 
-    <div class="basis-7/12 flex flex-col justify-center pr-16">
-      <div class="text-lg">#дизайн #uiux #figma</div>
+<!--    TODO-->
+<!--    <CourseCover class="sticky relative top-0 w-5/12 opacity-95"/>-->
+
+    <div class="w-7/12 pr-16 py-12">
+      <div class="text-lg ">#дизайн #uiux #figma</div>
       <h1 class="text-7xl">
         Як стартувати кар’єру ui/ux дизайнером
       </h1>
 
-      <div class="text-xl mt-16">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+      <div class="text-xl mt-8">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
       </div>
 
-      <div class="h-36 bg-violet-100 w-[20vw] mx-auto mt-12">
-        sososo
+      <div class="flex flex-row h-36 w-[50%] mx-auto mt-12 divide-x">
+        <div class="basis-1/3 flex justify-center items-center">
+          <div class="text-center">
+            3.5<br>
+            місяців
+          </div>
+        </div>
+        <div class="basis-1/3 flex justify-center items-center">
+          <div class="text-center">
+            88<br>
+            уроків
+          </div>
+        </div>
+        <div class="basis-1/3 flex justify-center items-center">
+          <div class="text-center">
+            88<br>
+            уроків
+          </div>
+        </div>
       </div>
 
       <button class="block w-2/3 mx-auto px-4 my-12 h-[60px] font-normal rounded-md bg-indigo-500">
@@ -31,6 +52,6 @@
 
 <script setup lang="ts">
 definePageMeta({
-  layout: 'clear',
+  layout: 'clear'
 });
 </script>
