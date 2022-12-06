575394bbed053af898333590316c286aa9d1f91c
Anna Romaniuk <80681158+AnechkaR@users.noreply.github.com>
2022-12-06 16:11:16 +0200

    Add links on login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="min-h-[300vh] container mx-auto">
+  <div class="min-h-[300vh] container mx-auto flex flex-col">
     <h1 class="text-2xl font-bold text-indigo-500">Hello World</h1>
 
     <NuxtLink to="/login">Login</NuxtLink>
+
+    <NuxtLink to="/courses/advanced-uiux-design-in-figma">Course landing</NuxtLink>
+    <NuxtLink to="/anna.romaniuk">Profile</NuxtLink>
+    <NuxtLink to="/lessons/515151/edit">Edit lesson</NuxtLink>
   </div>
 </template>
 
