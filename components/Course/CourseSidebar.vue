66ef10ac449b2dd6c752e18e5fb1701a1c1d0318
Anna Romaniuk <80681158+AnechkaR@users.noreply.github.com>
2022-12-06 16:13:11 +0200

    Add course lessons page

---
+++ components/Course/CourseSidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <NuxtLink
+      active-class="active"
+      :to="lessonsUrl"
+    >
+      lessons
+    </NuxtLink>
+    <NuxtLink
+      active-class="active"
+      :to="ratingUrl"
+    >
+      rating
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  courseSlug: string
+}>();
+
+const lessonsUrl = computed(() => `/courses/${props.courseSlug}/lessons`);
+const ratingUrl = computed(() => `/courses/${props.courseSlug}/rating`);
+</script>
+
+<style scoped>
+.active {
+  @apply bg-red-500;
+}
+</style>
--- pages/courses/[slug]/lessons.vue
+++ pages/courses/[slug]/lessons.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    {{ $route.params.slug }} course lessons
+  <div class="flex flex-row h-full space-x-5">
+    <div class="basis-2/12 h-full bg-gray-100">
+      <CourseSidebar :course-slug="$route.params.slug"/>
+    </div>
+
+    <div class="basis-10/12 bg-gray-100">
+      <div class="flex flex-col mt-4 m-2 space-y-2">
+        <div
+          v-for="id in 4"
+          class="flex flex bg-white pr-2 border rounded"
+        >
+          <div class="bg-violet-100 h-20 w-16"></div>
+
+          <div class="ml-2">
+            <div class="font-semibold">1.{{ id }} Auto layout</div>
+            <div class="text-gray-500">Learn new powerful techniques to create amazing user experience</div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
-
-<style scoped>
-
-</style>
