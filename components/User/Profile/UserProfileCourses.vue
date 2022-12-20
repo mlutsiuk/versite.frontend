1fddbc18f69921ab0b4ef9e821caf172b79d0141
Anna Romaniuk <80681158+AnechkaR@users.noreply.github.com>
2022-12-20 15:53:20 +0200

    Fix profile courses

--- components/User/Profile/UserProfileCourses.vue
+++ components/User/Profile/UserProfileCourses.vue
@@ -6,22 +6,20 @@
 
     <div class="grid grid-cols-2 gap-4 mt-4">
       <NuxtLink
-        v-for="id in 4"
         v-slot="{ navigate }"
         to="/courses/advanced-uiux-design-in-figma"
         custom
       >
         <div
           @click="navigate"
-          class="flex flex pr-2 border rounded select-none cursor-pointer"
+          class="flex flex-row py-2 pl-6 pr-2 border rounded select-none items-start cursor-pointer"
         >
-          <div class="bg-violet-100 h-20 w-16"></div>
-
-          <div class="ml-2">
+          <div class="flex flex-col justify-between">
             <div class="font-semibold">Advanced UI/UX Design in Figma</div>
-            <div class="text-gray-500">Learn new powerful techniques to create amazing user experience</div>
+            <div class="text-gray-500">Learn new powerful techniques to create amazing user experience.</div>
           </div>
 
+          <div class="bg-[#4caf50] text-white rounded-full p-2 ml-2">Завершений</div>
         </div>
       </NuxtLink>
     </div>
