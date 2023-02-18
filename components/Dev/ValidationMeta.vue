04b0c784d2a98f5447aff38e799deadf9723ece2
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-02-18 22:38:00 +0200

    feat: Add DevValidationMeta component for field validation debug

---
+++ components/Dev/ValidationMeta.vue
@@ -0,0 +1,22 @@
+<template>
+  <span
+    v-if="devStore.validation.showIsValid"
+    class="px-1 rounded text-white select-none"
+    :class="[
+      meta.valid ? 'bg-green-500' : 'bg-red-500'
+    ]"
+  >
+      {{ meta.valid ? 'valid' : 'invalid' }}
+  </span>
+</template>
+
+<script setup lang="ts">
+import { FieldMeta } from "vee-validate";
+import { useDevStore } from "~/store/dev";
+
+const props = defineProps<{
+  meta: FieldMeta<any>
+}>();
+
+const devStore = useDevStore();
+</script>
