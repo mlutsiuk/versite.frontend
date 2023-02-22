ccf34404d4d3b196718cc0dec5335c31a83995e9
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-02-22 05:02:02 +0200

    style: Code cleanup

--- types/validation/interaction-modes.ts
+++ types/validation/interaction-modes.ts
@@ -5,13 +5,13 @@ type InteractionEventGetter = (ctx: FieldContext<any>) => string[];
 // Validates on submit only
 const passive = () => [];
 
-const lazy: InteractionEventGetter = ({ meta, errorMessage }) => {
+const lazy: InteractionEventGetter = () => {
   return ['change'];
 };
 
 const aggressive: InteractionEventGetter = () => ['input'];
 
-const eager: InteractionEventGetter = ({ meta, errorMessage }) => {
+const eager: InteractionEventGetter = ({ errorMessage }) => {
   if (errorMessage.value) {
     return ['change', 'input'];
   }
