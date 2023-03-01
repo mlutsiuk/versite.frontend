9d92f6a892f7cea76f4d174a32896ed2311d7b5f
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-03-01 14:13:37 +0200

    feat: Add link to index in header logo

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="sticky top-0 z-50 w-full bg-white py-3 px-4 shadow-sm">
     <div class="relative flex justify-between items-center">
-      <div class="text-2xl select-none cursor-pointer">
-        Versite
-      </div>
+      <NuxtLink
+        v-slot="{ navigate }"
+        to="/"
+        custom
+      >
+        <div @click="navigate" class="text-2xl select-none cursor-pointer">
+          Versite
+        </div>
+      </NuxtLink>
 
       <div class="flex items-center">
         <HeaderChatsButton class="mr-3"/>
