c601fada3eee93ea6861d5fcb641705dad7c68c0
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-03-11 01:16:13 +0200

    dev(api): Remove is_registration_completed field form UserModel

--- api/user/models/UserModel.ts
+++ api/user/models/UserModel.ts
@@ -5,8 +5,7 @@ export type UserModel = {
   nickname?: string,
   email: string,
   avatar?: string,
-  email_verified_at?: Date,
-  is_registration_completed: boolean,
+  email_verified_at?: Date
 
   // roles: RelationCollection<RoleModel> | null,
   // permissions: RelationCollection<PermissionModel> | null
