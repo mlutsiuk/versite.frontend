a82e61c6f78edda00749920860563b92084f6bb4
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-03-11 04:12:44 +0200

    feat(api): Add strict checks for body and routeParams properties

--- api/_general/endpoint.ts
+++ api/_general/endpoint.ts
@@ -1,35 +1,50 @@
 import { RouterMethod } from 'h3';
 import { AsyncHttpOptions } from '~/types/fetch/fetch';
 
+// interface PaParams<ResT, ReqT, RouteParamsT> extends Omit<AsyncHttpOptions<ResT>, 'method' | 'body'> {
+//   body: ReqT extends undefined ? never : ReqT,
+//   routeParams?: RouteParamsT extends undefined ? undefined : RouteParamsT
+// }
+
+interface PaParams<ResT> extends Omit<AsyncHttpOptions<ResT>, 'method' | 'body'> {
+  // body: ReqT extends undefined ? never : ReqT,
+  // routeParams?: RouteParamsT extends undefined ? undefined : RouteParamsT
+}
+
+type BPaParams<ResT, ReqT> = ReqT extends undefined    // TODO: Create helper type
+  ? PaParams<ResT> & { body?: never }
+  : PaParams<ResT> & { body: ReqT};
+
+type RBPaParams<ResT, ReqT, RouteParamsT> = RouteParamsT extends undefined
+  ? BPaParams<ResT, ReqT> & { routeParams?: never }
+  : BPaParams<ResT, ReqT> & { routeParams: RouteParamsT };
+
 export class Endpoint<
   ResT,
-  ReqT extends Record<string, any> | never = never,
-  UrlParamsT = undefined
+  ReqT extends Record<string, any> | undefined = undefined,
+  RouteParamsT = undefined
 > {
   private readonly method;
-  private readonly url: ((params: UrlParamsT) => string) | string;
+  private readonly url: ((params: RouteParamsT) => string) | string;
 
   constructor(opts: {
     method: Uppercase<RouterMethod>,
-    url: UrlParamsT extends undefined ? string : (params: UrlParamsT) => string
+    url: RouteParamsT extends undefined ? string : (params: RouteParamsT) => string
   }) {
     this.method = opts.method;
     this.url = opts.url;
   }
 
   public asyncData(
-    body?: ReqT,
-    options?: Omit<AsyncHttpOptions<ResT>, 'method' | 'body'>,
-    urlParams?: UrlParamsT
+    options: RBPaParams<ResT, ReqT, RouteParamsT>
   ) {
 
     let url = this.url instanceof Function
-      ? this.url(urlParams)
+      ? this.url(options.routeParams as RouteParamsT)    // TODO: Check for undefined
       : this.url;
 
     return useAsyncHttp<ResT>(url, {
       method: this.method,
-      body: body,
 
       ...options
     });
