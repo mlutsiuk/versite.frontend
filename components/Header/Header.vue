c4da59958775fcd4255ae2326cb512160efc5506
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-03-15 23:18:42 +0200

    fix: Header logo click behaviour

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -2,13 +2,10 @@
   <div class="sticky top-0 z-50 w-full bg-white py-3 px-4 shadow-sm">
     <div class="relative flex justify-between items-center">
       <NuxtLink
-        v-slot="{ navigate }"
+        class="text-2xl select-none cursor-pointer"
         to="/"
-        custom
       >
-        <div @click="navigate" class="text-2xl select-none cursor-pointer">
-          Versite
-        </div>
+        Versite
       </NuxtLink>
 
       <div class="flex items-center">
