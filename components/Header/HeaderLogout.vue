795057f44b81c10c6828339a9a6f98a20dca0966
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-03-16 16:31:30 +0200

    feat: Add logout button

---
+++ components/Header/HeaderLogout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hover:bg-gray-200 rounded-full cursor-pointer transition-all p-[4px]">
+    <Icon
+      class="text-gray-600"
+      name="mdi:logout-variant"
+      size="28"
+      @click="logout"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useAuthStore } from '~/store/auth';
+
+function logout() {
+  useAuthStore().logout();
+
+  navigateTo('/login');
+}
+</script>
