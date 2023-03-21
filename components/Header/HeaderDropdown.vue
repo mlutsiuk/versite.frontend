379af26f23bbcd8af0fbddc8f3de80e8a0d74570
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-03-21 19:38:31 +0200

    fix: HeaderDropdown redundant margin

--- components/Header/HeaderDropdown.vue
+++ components/Header/HeaderDropdown.vue
@@ -13,43 +13,45 @@
       leave-to-class="translate-y-1 opacity-0"
     >
       <PopoverPanel
-        class="absolute right-0 z-10 mt-4 shadow-lg bg-white rounded-md space-y-2.5 p-3"
+        class="absolute right-0 z-10 mt-4 shadow-lg bg-white rounded-md p-3"
       >
-        <div class="flex flex-row items-end">
-          <HeaderAvatar size="52px"/>
+        <div class="space-y-2.5">
+          <div class="flex flex-row items-end">
+            <HeaderAvatar size="52px"/>
 
-          <div class="ml-2">
-            <div
-              v-if="user?.nickname"
-              class="text-gray-800 text-xs"
-              v-text="`@${user?.nickname}`"
-            />
-            <div class="whitespace-nowrap" v-text="user?.name"/>
+            <div class="ml-2">
+              <div
+                v-if="user?.nickname"
+                class="text-gray-800 text-xs"
+                v-text="`@${user?.nickname}`"
+              />
+              <div class="whitespace-nowrap" v-text="user?.name"/>
+            </div>
           </div>
-        </div>
 
-        <div class="separator"></div>
+          <div class="separator"></div>
 
-        <div class="dropdown-item">
-          <Icon name="mdi:user-edit" size="24"/>
-          <span>Редагувати профіль</span>
-        </div>
+          <div class="dropdown-item">
+            <Icon name="mdi:user-edit" size="24"/>
+            <span>Редагувати профіль</span>
+          </div>
 
-        <div class="dropdown-item">
-          <Icon name="material-symbols:translate" size="24"/>
-          <span>Мова інтерфейсу</span>
-        </div>
+          <div class="dropdown-item">
+            <Icon name="material-symbols:translate" size="24"/>
+            <span>Мова інтерфейсу</span>
+          </div>
 
-        <div class="dropdown-item">
-          <Icon name="mdi:sun-moon-stars" size="24"/>
-          <span>Тема</span>
-        </div>
+          <div class="dropdown-item">
+            <Icon name="mdi:sun-moon-stars" size="24"/>
+            <span>Тема</span>
+          </div>
 
-        <div class="separator"></div>
+          <div class="separator"></div>
 
-        <div class="dropdown-item" @click="logout">
-          <Icon name="material-symbols:logout" size="24"/>
-          <span>Вийти</span>
+          <div class="dropdown-item" @click="logout">
+            <Icon name="material-symbols:logout" size="24"/>
+            <span>Вийти</span>
+          </div>
         </div>
       </PopoverPanel>
     </transition>
