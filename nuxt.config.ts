acea16e457171db326a5a1928df903e6ca71615c
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-04 00:18:22 +0300

    dev: Add nuxt-typed-router nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
     transpile: ['@headlessui/vue']
   },
   modules: [
+    'nuxt-typed-router',
     '@nuxt/devtools',
     [
       '@nuxtjs/google-fonts',
@@ -17,10 +18,10 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt'
   ],
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
-    layoutTransition: { name: 'layout', mode: 'out-in' }
-  },
+  // app: {
+  //   pageTransition: { name: 'page', mode: 'out-in' },
+  //   layoutTransition: { name: 'layout', mode: 'out-in' }
+  // },
   css: [
     '@/assets/scss/index.scss'
   ],
