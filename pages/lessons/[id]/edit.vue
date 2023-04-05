e3fc5aad0f3571b99bc8c5e039977e4d3ecde5ad
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-06 02:28:55 +0300

    feat: Add assignments column to lesson edit page

--- pages/lessons/[id]/edit.vue
+++ pages/lessons/[id]/edit.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="pt-4 w-full flex flex-col items-center">
-    <LessonEditor class="w-full max-w-6xl"/>
+  <div class="flex w-full flex-row space-x-5">
+    <div class="flex w-9/12 flex-row items-center">
+      <LessonEditor class="w-full" />
+    </div>
+
+    <div class="h-[30rem] w-3/12 rounded bg-white shadow">
+      <h2 class="p-4 text-2xl">Завдання</h2>
+    </div>
   </div>
 </template>
