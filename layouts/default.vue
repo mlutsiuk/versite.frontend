3704e82e9719348aa63a2be62c036f300819bd09
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-07 18:08:56 +0300

    feat: Change default layout background color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="min-h-screen flex flex-col bg-neutral-50">
-    <Header/>
+  <div class="flex min-h-screen flex-col bg-neutral-100">
+    <Header />
 
-    <main class="container mx-auto mt-8 mb-4 flex flex-grow">
-      <slot/>
+    <main class="container mx-auto mb-4 mt-8 flex flex-grow">
+      <slot />
     </main>
   </div>
 </template>
