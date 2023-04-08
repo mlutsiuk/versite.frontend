dbf2b0c5424f094ae476bfb94fb94535f53e3e40
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-08 16:48:36 +0300

    dev: Add DevDataId component for displaying resource in

---
+++ components/Dev/DataId.vue
@@ -0,0 +1,18 @@
+<template>
+  <span
+    v-if="devStore.data.showId"
+    class="select-none rounded bg-red-400 px-1 text-white"
+  >
+    Id: {{ props.id }}
+  </span>
+</template>
+
+<script lang="ts" setup>
+import { useDevStore } from '~/store/dev';
+
+const props = defineProps<{
+  id: number | string;
+}>();
+
+const devStore = useDevStore();
+</script>
--- store/dev.ts
+++ store/dev.ts
@@ -1,13 +1,19 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia';
 
 interface State {
+  data: {
+    showId: boolean;
+  };
   validation: {
-    showIsValid: boolean
-  }
+    showIsValid: boolean;
+  };
 }
 
-export const useDevStore = defineStore("dev", {
+export const useDevStore = defineStore('dev', {
   state: (): State => ({
+    data: {
+      showId: true
+    },
     validation: {
       showIsValid: false
     }
