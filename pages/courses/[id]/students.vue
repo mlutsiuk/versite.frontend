064245a5b2adb3a26488c4eceb3a785886d83efc
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-08 16:55:04 +0300

    feat: Add courses/[id] nested pages rating, students and unchecked-assignments

--- pages/courses/[id]/students.vue
+++ pages/courses/[id]/students.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div class="flex items-center space-x-2">
-      <h2 class="text-xl font-medium">Рейтинг</h2>
+      <h2 class="text-xl font-medium">Студенти</h2>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const route = useRoute('courses-id-rating');
+const route = useRoute('courses-id-students');
 </script>
---
+++ pages/courses/[id]/unckecked-assignments.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="flex items-center space-x-2">
+      <h2 class="text-xl font-medium">Завдання на перевірку</h2>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute('courses-id-unckecked-assignments');
+</script>
