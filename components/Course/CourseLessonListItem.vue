7631a602784c660418fbea4d7b18116a64f898ee
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-08 16:56:03 +0300

    feat: Add course lessons output draft

---
+++ components/Course/CourseLessonListItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="text-lg font-medium">
+      {{ lesson.title }}
+    </div>
+    <div
+      class="mb-2 flex flex-row items-center space-x-2 rounded text-gray-600"
+    >
+      <Icon class="text-gray-600" name="mdi:calendar" />
+      <span v-text="date" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import dayjs from 'dayjs';
+import { Lesson } from '~/api/lesson';
+
+const props = defineProps<{
+  lesson: Lesson;
+}>();
+
+const date = computed(() => {
+  return dayjs.unix(props.lesson.date).format('DD.MM.YYYY HH:mm');
+});
+</script>
--- pages/courses/[id]/lessons.vue
+++ pages/courses/[id]/lessons.vue
@@ -1,28 +1,23 @@
 <template>
-  <div class="flex flex-row h-full space-x-5">
-    <div class="basis-2/12 h-full bg-gray-100">
-      <CourseSidebar :course-slug="$route.params.id"/>
+  <div>
+    <div class="flex items-center space-x-2">
+      <h2 class="text-xl font-medium">Уроки</h2>
     </div>
 
-    <div class="basis-10/12 bg-gray-100">
-      <div class="flex flex-col mt-4 m-2 space-y-2">
-        <div
-          v-for="id in 4"
-          class="flex flex bg-white pr-2 border rounded"
-        >
-          <div class="bg-violet-100 h-20 w-16"></div>
-
-          <div class="ml-2">
-            <div class="font-semibold">1.{{ id }} Auto layout</div>
-            <div class="text-gray-500">Learn new powerful techniques to create amazing user experience</div>
-          </div>
-
-        </div>
-      </div>
+    <div v-if="lessons">
+      <CourseLessonList :lessons="lessons.data" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { lessons as lessonRepository } from '~/api/repositories';
 
+const route = useRoute('courses-id-lessons');
+
+const { data: lessons } = await lessonRepository.allFromCourse.asyncData({
+  routeParams: {
+    id: route.params.id
+  }
+});
 </script>
