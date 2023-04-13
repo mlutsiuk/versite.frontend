e90ed4581f77a77b039d342cb43636dc3d895831
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:50:08 +0300

    feat: Restyle and add skeleton loaders to CourseSidebarNavigation

--- components/Course/CourseSidebarNavigation.vue
+++ components/Course/CourseSidebarNavigation.vue
@@ -1,75 +1,78 @@
 <template>
-  <div class="rounded bg-white">
-    <div class="px-3.5 py-2.5 text-2xl font-medium">
-      {{ props.courseTitle }}
+  <div class="flex flex-col space-y-2 rounded bg-white p-2">
+    <div v-if="props.courseTitle">
+      <div class="text-sm text-gray-400">Курс</div>
+      <div class="text-2xl font-medium" v-text="props.courseTitle"/>
+    </div>
+    <div v-else>
+      <SkeletonBone class="h-3 w-16 rounded-full mt-1" />
+      <SkeletonBone class="h-6 w-10/12 rounded-full mt-2" />
     </div>
 
     <hr />
 
-    <div class="flex flex-col space-y-2 p-2">
-      <NuxtLink
-        :to="{
-          name: 'courses-id-lessons',
-          params: { id: props.courseId }
-        }"
-        active-class="active"
-        class="link"
-      >
-        <Icon name="mdi:notebook-outline" size="24" />
-        <span>Уроки</span>
-      </NuxtLink>
+    <NuxtLink
+      :to="{
+        name: 'courses-id-lessons',
+        params: { id: props.courseId }
+      }"
+      active-class="active"
+      class="link"
+    >
+      <Icon name="mdi:notebook-outline" size="24" />
+      <span>Уроки</span>
+    </NuxtLink>
 
-      <NuxtLink
-        :to="{
-          name: 'courses-id-students',
-          params: { id: props.courseId }
-        }"
-        active-class="active"
-        class="link"
-      >
-        <Icon name="mdi:account-group-outline" size="24" />
-        <span>Студенти</span>
-      </NuxtLink>
+    <NuxtLink
+      :to="{
+        name: 'courses-id-students',
+        params: { id: props.courseId }
+      }"
+      active-class="active"
+      class="link"
+    >
+      <Icon name="mdi:account-group-outline" size="24" />
+      <span>Студенти</span>
+    </NuxtLink>
 
-      <NuxtLink
-        :to="{
-          name: 'courses-id-rating',
-          params: { id: props.courseId }
-        }"
-        active-class="active"
-        class="link"
-      >
-        <Icon name="mdi:file-chart-outline" size="24" />
-        <span>Рейтинг</span>
-      </NuxtLink>
+    <NuxtLink
+      :to="{
+        name: 'courses-id-rating',
+        params: { id: props.courseId }
+      }"
+      active-class="active"
+      class="link"
+    >
+      <Icon name="mdi:file-chart-outline" size="24" />
+      <span>Рейтинг</span>
+    </NuxtLink>
 
-      <hr />
+    <hr />
 
-      <NuxtLink
-        :to="{
-          name: 'courses-id-unckecked-assignments',
-          params: { id: props.courseId }
-        }"
-        active-class="active"
-        class="link"
-      >
-        <Icon name="mdi:text-box-check-outline" size="24" />
-        <span>Завдання</span>
-      </NuxtLink>
-    </div>
+    <NuxtLink
+      :to="{
+        name: 'courses-id-unckecked-assignments',
+        params: { id: props.courseId }
+      }"
+      active-class="active"
+      class="link"
+    >
+      <Icon name="mdi:text-box-check-outline" size="24" />
+      <span>Завдання</span>
+    </NuxtLink>
   </div>
 </template>
 
 <script lang="ts" setup>
 const props = defineProps<{
   courseId: string;
-  courseTitle: string;
+  courseTitle?: string;
 }>();
 </script>
 
 <style scoped>
 .link {
-  @apply cursor-pointer space-x-4 whitespace-nowrap rounded-sm px-1.5 py-3 text-xl transition-colors hover:bg-gray-200;
+  @apply cursor-pointer space-x-4 whitespace-nowrap rounded px-1.5 py-3 text-xl transition-colors hover:bg-gray-200;
 }
 
 .active {
