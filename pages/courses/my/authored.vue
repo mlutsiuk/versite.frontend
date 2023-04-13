31349359f54c72bc108e658479a1b444b31817d7
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:56:22 +0300

    feat: Add page for user authored courses

---
+++ pages/courses/my/authored.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="space-y-2.5 px-5 py-2.5">
+    <div class="flex flex-row items-center">
+      <h2 class="text-xl font-medium leading-8">Авторські курси</h2>
+    </div>
+
+    <hr />
+
+    <div v-if="pending" class="grid grid-cols-3 gap-3">
+      <div v-for="i in 4" :key="i">
+        <CourseCardSkeleton />
+      </div>
+    </div>
+    <div v-else-if="error">
+      <h2 class="text-3xl">ERRORRRR</h2>
+      {{ error }}
+    </div>
+    <div v-else-if="data" class="grid grid-cols-3 gap-3">
+      <NuxtLink
+        v-for="course in data.data"
+        :key="course.id"
+        :to="{
+          name: 'courses-id-lessons',
+          params: { id: course.id }
+        }"
+      >
+        <CourseCard :course="course" />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { courses } from '~/api/repositories';
+
+const { data, error, pending, execute } = await courses.authored.asyncData({
+  key: 'courses:authored',
+  immediate: false,
+  params: {
+    include: 'author,lessons,next_lesson'
+  }
+});
+
+onMounted(execute);
+</script>
