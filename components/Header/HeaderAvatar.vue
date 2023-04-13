5b1c3f50a9c02396793018c1e1ba9568de4ae226
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:37:45 +0300

    feat: Replace HeaderAvatar with UserAvatar

--- components/Header/HeaderAvatar.vue
+++ components/Header/HeaderAvatar.vue
@@ -1,16 +1,9 @@
 <template>
-  <div :style="{ width: props.size, height: props.size }" class="select-none">
-    <img v-if="user?.avatar" :src="user?.avatar" alt="Avatar" class="avatar" />
-    <div
-      v-else-if="user"
-      :style="{ 'background-color': avatarColor }"
-      class="missing-avatar"
-    >
-      <div>
-        {{ user!.name.charAt(0).toUpperCase() }}
-      </div>
-    </div>
-  </div>
+  <UserAvatar
+    :size="props.size"
+    :user="user"
+    class="cursor-pointer rounded-md"
+  />
 </template>
 
 <script setup lang="ts">
@@ -27,34 +20,4 @@ const props = withDefaults(
 
 const authStore = useAuthStore();
 const user = computed(() => authStore.user);
-
-const avatarColor = computed(() => {
-  if (!user.value) {
-    return;
-  }
-
-  let hash = 0;
-  for (let i = 0; i < user.value!.nickname.length; i++) {
-    hash = user.value!.nickname.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  console.info(hash);
-
-  const h = hash % 360;
-  const s = 40;
-  const l = 65;
-  return `hsl(${h}, ${s}%, ${l}%)`;
-});
 </script>
-
-<style scoped>
-.avatar {
-  @apply cursor-pointer
-  rounded-md;
-}
-
-.missing-avatar {
-  @apply flex h-full w-full items-center justify-center rounded-md
-  text-2xl font-medium text-white
-  transition-colors;
-}
-</style>
