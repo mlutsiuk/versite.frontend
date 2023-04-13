646ffd1b73607777340d269d93245eeb68d4a5f9
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:59:52 +0300

    feat: Sidebar navigation for courses/my pages

---
+++ components/Course/CourseMySidebarNavigation.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex flex-col space-y-2 rounded bg-white p-2">
+    <NuxtLink
+      :to="{
+        name: 'courses-my-participated'
+      }"
+      active-class="active"
+      class="link"
+    >
+      <Icon name="mdi:school-outline" size="24" />
+      <span>Мої курси</span>
+    </NuxtLink>
+
+    <NuxtLink
+      :to="{
+        name: 'courses-my-authored'
+      }"
+      active-class="active"
+      class="link"
+    >
+      <Icon name="mdi:human-male-board" size="24" />
+      <span>Авторські курси</span>
+    </NuxtLink>
+
+    <hr />
+
+    <NuxtLink
+      :to="{
+        name: 'courses-my-create'
+      }"
+      active-class="active"
+      class="link"
+    >
+      <Icon name="mdi:plus" size="24" />
+      <span>Створити</span>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.link {
+  @apply cursor-pointer space-x-4 whitespace-nowrap rounded px-1.5 py-3 text-xl transition-colors hover:bg-gray-200;
+}
+
+.active {
+  @apply bg-gray-100;
+}
+</style>
