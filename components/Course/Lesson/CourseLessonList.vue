6b7338a18550e1a5168df44ee5bb3a91bc6a2646
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:48:03 +0300

    feat: Style CourseLesson* components

---
+++ components/Course/Lesson/CourseLessonList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="space-y-2.5">
+    <NuxtLink
+      v-for="lesson in props.lessons"
+      :key="lesson.id"
+      :to="{
+        name: 'lessons-id',
+        params: { id: lesson.id }
+      }"
+      class="block"
+    >
+      <CourseLessonListItem :lesson="lesson" />
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Lesson } from '~/api/lesson';
+
+const props = defineProps<{
+  lessons: Lesson[];
+}>();
+</script>
---
+++ components/Course/Lesson/CourseLessonListItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="rounded-sm bg-neutral-100 px-2 py-3 transition-colors hover:bg-neutral-200"
+  >
+    <DevOnly>
+      <DevDataId :id="lesson.id" />
+    </DevOnly>
+
+    <div class="mb-1 text-xl font-medium">
+      {{ lesson.title }}
+    </div>
+
+    <div class="mb-2 flex flex-row items-center space-x-2 text-gray-600">
+      <Icon class="text-gray-600" name="mdi:calendar" />
+      <span v-text="date" />
+    </div>
+
+    <div class="text-sm font-medium">
+      <div class="text-gray-400">Завдання</div>
+
+      <ul class="pl-2">
+        <li class="list-inside list-disc">
+          Create a simple login screen for a mobile app.
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import dayjs from 'dayjs';
+import { Lesson } from '~/api/lesson';
+
+const props = defineProps<{
+  lesson: Lesson;
+}>();
+
+const date = computed(() => {
+  return dayjs.unix(props.lesson.date).format('DD.MM.YYYY HH:mm');
+});
+</script>
