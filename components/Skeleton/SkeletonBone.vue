06530f05d3fe98c4cef97d16560bd0c996ea09aa
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:38:29 +0300

    feat: Create SkeletonBone component

---
+++ components/Skeleton/SkeletonBone.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="skeleton-bone"></div>
+</template>
+
+<style lang="scss" scoped>
+.skeleton-bone {
+  @apply relative overflow-hidden bg-neutral-200;
+
+  animation: skeleton-bone 1.5s ease-in-out infinite;
+
+  &::after {
+    animation: loading 1.5s infinite;
+    content: '';
+    height: 100%;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transform: translateX(-100%);
+    z-index: 1;
+
+    background: linear-gradient(
+      90deg,
+      rgba(#fff, 0),
+      rgba(#fff, 0.3),
+      rgba(#fff, 0)
+    );
+  }
+}
+
+@keyframes loading {
+  100% {
+    transform: translateX(100%);
+  }
+}
+</style>
