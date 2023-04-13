846b284f0130fdaddda5c6e8bf57709bbdc7c4a4
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:54:55 +0300

    feat: Refactor course/[id]/students page

--- pages/courses/[id]/students.vue
+++ pages/courses/[id]/students.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>
-    <div class="flex items-center space-x-2">
+  <div class="space-y-2.5 px-5 py-2.5">
+    <div class="flex flex-row items-center justify-between">
       <h2 class="text-xl font-medium">Студенти</h2>
+
+      <div
+        class="flex cursor-pointer select-none flex-row rounded bg-gray-100 px-2 py-1 transition-colors hover:bg-gray-200"
+      >
+        <Icon class="mr-1" name="mdi:plus" size="24px" />
+
+        Створити
+      </div>
+    </div>
+
+    <hr />
+
+    <div>
+      <CourseStudentsListSkeleton v-if="pending" />
+      <CourseStudentsList v-else-if="data" :students="data.data" />
+      <div v-else v-text="error" />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { students } from '~/api/repositories';
+
 const route = useRoute('courses-id-students');
+
+const { data, error, execute, pending } =
+  await students.allFromCourse.asyncData({
+    immediate: false,
+    key: 'students:find',
+    routeParams: {
+      id: route.params.id
+    },
+    query: {
+      include: 'user,invitation.receiver'
+    }
+  });
+
+onMounted(execute);
 </script>
