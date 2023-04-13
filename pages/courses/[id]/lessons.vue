9497d3459bb20f2c24cd672f21104c32e1446c8a
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:54:42 +0300

    feat: Refactor course/[id]/lessons page

--- pages/courses/[id]/lessons.vue
+++ pages/courses/[id]/lessons.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
-    <div class="flex items-center space-x-2">
+  <div class="space-y-2.5 px-5 py-2.5">
+    <div class="flex flex-row items-center justify-between">
       <h2 class="text-xl font-medium">Уроки</h2>
+
+      <div
+        class="flex cursor-pointer select-none flex-row rounded bg-gray-100 px-2 py-1 transition-colors hover:bg-gray-200"
+      >
+        <Icon class="mr-1" name="mdi:plus" size="24px" />
+
+        Створити
+      </div>
     </div>
 
-    <div v-if="lessons">
-      <CourseLessonList :lessons="lessons.data" />
+    <hr />
+
+    <div>
+      <h2 class="mb-1 text-xl">Майбутні</h2>
+
+      <CourseLessonListSkeleton v-if="pending" />
+      <CourseLessonList v-else-if="lessons" :lessons="lessons.data" />
+      <div v-else v-text="error" />
     </div>
   </div>
 </template>
@@ -15,13 +29,17 @@ import { lessons as lessonRepository } from '~/api/repositories';
 
 const route = useRoute('courses-id-lessons');
 
-const { data: lessons, execute } =
-  await lessonRepository.allFromCourse.asyncData({
-    immediate: false,
-    routeParams: {
-      id: route.params.id
-    }
-  });
+const {
+  data: lessons,
+  error,
+  pending,
+  execute
+} = await lessonRepository.allFromCourse.asyncData({
+  immediate: false,
+  routeParams: {
+    id: route.params.id
+  }
+});
 
 onMounted(() => {
   execute();
