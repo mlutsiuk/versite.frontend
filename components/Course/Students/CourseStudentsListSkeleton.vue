18daf6786f29f495c3f13a6b4c160b5e2f4780ab
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:52:21 +0300

    feat: Add skeleton loader for CourseStudentsList component

---
+++ components/Course/Students/CourseStudentsListSkeleton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col space-y-2.5">
+    <div
+      v-for="i in 4"
+      :key="i"
+      class="flex flex-row space-x-2.5 rounded bg-neutral-100 p-2.5"
+    >
+      <SkeletonBone class="h-[50px] w-[50px] rounded-full" />
+
+      <div class="grow">
+        <div class="mb-2 flex flex-row items-center justify-between">
+          <SkeletonBone class="h-4 w-52 rounded-full" />
+
+          <Icon class="text-neutral-200" name="mdi:dots-vertical" size="24px" />
+        </div>
+        <SkeletonBone class="h-2.5 w-36 rounded-full" />
+      </div>
+    </div>
+  </div>
+</template>
