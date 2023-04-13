2c5bf36528608e2822b696314b95de7b8622a3ce
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:43:39 +0300

    feat: Add lesson/[id] page blank

---
+++ pages/lessons/[id]/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex w-full flex-row space-x-5">
+    <div class="flex w-9/12 flex-row items-center">
+      Перегляд уроку - {{ route.params.id }}
+    </div>
+
+    <div class="h-[30rem] w-3/12 rounded bg-white shadow">
+      <h2 class="p-4 text-2xl">Завдання</h2>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute('lessons-id');
+</script>
