f47743ab28448f275ed8b469d2866438c2424c70
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:59:29 +0300

    feat: Add user participated courses page blank

---
+++ pages/courses/my/participated.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="space-y-2.5 px-5 py-2.5">
+    <div class="flex flex-row items-center">
+      <h2 class="text-xl font-medium leading-8">Мої курси</h2>
+    </div>
+
+    <hr />
+  </div>
+</template>
