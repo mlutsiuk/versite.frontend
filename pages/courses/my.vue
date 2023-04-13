07bac55ce44458ad60192be71efb9c4f36734174
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-13 16:59:11 +0300

    feat: Add parent page for course/my pages

---
+++ pages/courses/my.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex w-full flex-row space-x-5">
+    <div class="basis-1/5">
+      <CourseMySidebarNavigation class="w-full" />
+    </div>
+
+    <div class="basis-4/5 rounded bg-white">
+      <NuxtPage :page-key="$route.fullPath"></NuxtPage>
+    </div>
+  </div>
+</template>
