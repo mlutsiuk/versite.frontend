e36f672d74adb0884e24e0032b0880939fdd4edf
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-16 01:21:52 +0300

    fix: default layout scrollbar behaviour

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex h-screen flex-col bg-neutral-100">
+  <div class="flex h-screen min-h-screen flex-col bg-neutral-100">
     <Header />
 
-    <main class="container mx-auto flex grow overflow-hidden pb-4 pt-8">
+    <main class="container mx-auto flex flex-1 overflow-y-hidden pb-4 pt-8">
       <slot />
     </main>
   </div>
