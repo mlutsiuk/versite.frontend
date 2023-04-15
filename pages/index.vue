a13d007f01a008c92f954636a5d0da53b5a1c866
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-16 01:59:40 +0300

    feat: Redirect from index page to courses/my/participated

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="min-h-screen container mx-auto flex flex-col p-8">
+  <div class="container mx-auto flex min-h-screen flex-col p-8">
     <h1 class="text-2xl font-bold text-indigo-500">Hello World</h1>
 
     <NuxtLink to="/login">Login</NuxtLink>
 
     <NuxtLink to="/courses">Courses</NuxtLink>
-    <NuxtLink to="/courses/advanced-uiux-design-in-figma">Course landing</NuxtLink>
+    <NuxtLink to="/courses/advanced-uiux-design-in-figma">
+      Course landing
+    </NuxtLink>
     <NuxtLink to="/anna.romaniuk">Profile</NuxtLink>
     <NuxtLink to="/lessons/515151/edit">Edit lesson</NuxtLink>
   </div>
@@ -13,6 +15,8 @@
 
 <script setup lang="ts">
 definePageMeta({
-  layout: 'auth',
+  redirect: {
+    name: 'courses-my-participated'
+  }
 });
 </script>
