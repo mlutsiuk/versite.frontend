d29b897a7783c9759a0b3ecd6833fc4206ab1ef8
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-16 01:25:10 +0300

    fix: Add participated courses view page

--- pages/courses/my/participated.vue
+++ pages/courses/my/participated.vue
@@ -5,5 +5,40 @@
     </div>
 
     <hr />
+
+    <div v-if="pending" class="grid grid-cols-3 gap-3">
+      <div v-for="i in 4" :key="i">
+        <CourseCardSkeleton />
+      </div>
+    </div>
+    <div v-else-if="error">
+      <h2 class="text-3xl">ERRORRRR</h2>
+      {{ error }}
+    </div>
+    <div v-else-if="data" class="grid grid-cols-3 gap-3">
+      <NuxtLink
+        v-for="course in data.data"
+        :key="course.id"
+        :to="{
+          name: 'courses-id-lessons',
+          params: { id: course.id }
+        }"
+      >
+        <CourseCard :course="course" />
+      </NuxtLink>
+    </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { courses } from '~/api/repositories';
+
+const { data, error, pending, execute } = await courses.participated.asyncData({
+  immediate: false,
+  params: {
+    include: 'author,lessons,next_lesson'
+  }
+});
+
+onMounted(execute);
+</script>
