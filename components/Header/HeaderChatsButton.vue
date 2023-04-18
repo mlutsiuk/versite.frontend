ec191b5fb5e7542fcf815fe1693eeea1db606e24
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-18 18:23:01 +0300

    fix: Add hover state to header buttons

--- components/Header/HeaderChatsButton.vue
+++ components/Header/HeaderChatsButton.vue
@@ -1,7 +1,9 @@
 <template>
-  <Icon
-    class="cursor-pointer text-gray-600"
-    name="mdi:message-text-outline"
-    size="28"
-  />
+  <div class="p-1 hover:bg-gray-200 rounded-full transition-colors">
+    <Icon
+      class="cursor-pointer text-gray-600"
+      name="mdi:message-text-outline"
+      size="28"
+    />
+  </div>
 </template>
--- components/Header/Notifications/HeaderNotificationsButton.vue
+++ components/Header/Notifications/HeaderNotificationsButton.vue
@@ -1,7 +1,7 @@
 <template>
   <MdBadge content="6" color="bg-[#4CAF50]" class="cursor-pointer">
     <div
-      class="cursor-pointer rounded-full p-[4px] transition-all hover:bg-gray-200"
+      class="cursor-pointer rounded-full p-1 transition-all hover:bg-gray-200"
     >
       <Icon class="text-gray-600" name="mdi:bell-outline" size="28" />
     </div>
