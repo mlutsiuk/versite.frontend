4ce71477806c2fcdf273a49830b8e645c67101bd
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-06-19 01:44:19 +0300

    feat: Course create page styles

--- pages/courses/my/create.vue
+++ pages/courses/my/create.vue
@@ -1,17 +1,51 @@
 <template>
-  <div class="space-y-2.5 px-5 py-2.5">
+  <div class="grow space-y-2.5 px-5 py-2.5">
     <div class="flex flex-row items-center">
       <h2 class="text-xl font-medium leading-8">Створення курсу</h2>
     </div>
 
     <hr />
 
-    <TextField class="mb-3" name="title" placeholder="Title" />
-    <TextField class="mb-3" name="description" placeholder="Description" />
+    <div class="flex flex-row space-x-4">
+      <div class="basis-5/12">
+        <TextField class="mb-3" name="title" placeholder="Назва" />
+        <TextField class="mb-3" name="description" placeholder="Опис" />
 
-    <MdButton block class="text-[#3A84AD]" size="x-large" @click="createCourse">
-      Create
-    </MdButton>
+        <div
+          block
+          class="cursor-pointer space-x-4 whitespace-nowrap rounded bg-gray-100 px-1.5 py-3 text-center text-lg transition-colors hover:bg-gray-200"
+          @click="createCourse"
+        >
+          Створити
+        </div>
+      </div>
+
+      <div class="w-px bg-gray-300"></div>
+
+      <div class="basis-7/12">
+        <h1
+          :class="{
+            'text-gray-400': form.values.title?.length == 0
+          }"
+          class="text-6xl"
+        >
+          {{ form.values.title?.length == 0 ? 'Назва' : form.values.title }}
+        </h1>
+
+        <div
+          :class="{
+            'text-gray-400': form.values.description?.length == 0
+          }"
+          class="mt-8 text-xl"
+        >
+          {{
+            form.values.description?.length == 0
+              ? 'Опис'
+              : form.values.description
+          }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,7 +58,11 @@ const form = useForm<CreateCourseRequest>({
   validationSchema: object({
     title: string().min(4).max(32),
     description: string().min(15).max(100)
-  })
+  }),
+  initialValues: {
+    title: '',
+    description: ''
+  }
 });
 
 async function createCourse() {
